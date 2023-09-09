9a0c4a6f2afa210037be59fa43e920573d2b928d
masaratbagwan01 <masaratbagwan01@gmail.com>
2023-09-09 07:18:59 +0530

    Create 08_array.js
    array created

---
+++ 08_array.js
@@ -0,0 +1,4 @@
+let array = [10, 20, 30, 50, 10, 80, 20, null, "jenny", true , "king"];
+console.log(typeof array);
+console.log(array);
+console.log(`total number of element in array is : ${array}`);
